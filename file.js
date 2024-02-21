289bda6c3404d004bc56535b10d04f6dea485350
Allan <127093730+1Juubito@users.noreply.github.com>
2024-02-21 19:19:18 -0300

    Exercício

---
+++ file.js
@@ -0,0 +1,16 @@
+let idade;
+let nome = "Chester";
+
+idade=30;
+idade = idade+1;
+nome= "M Shadows"
+
+
+
+if(idade >=18){
+    console.log("Seja bem vindo");}
+
+    else{
+        console.log("Acesso bloqueado");
+
+    }
